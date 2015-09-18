9977160191550678b7cfd19e4f8682cf630a5142
thomas.pink <thomas.pink@netural.com>
2015-09-18 12:09:43 +0200

    more tests

--- test/utils.js
+++ test/utils.js
@@ -79,5 +79,118 @@ describe('Utils', function() {
             expect(random).to.be.a('string');
             expect(random).to.have.length(8);
         });
-    })
+    });
+    describe('isArray', function() {
+        it('should return true if param is an array', function () {
+            expect(Utils.isArray([])).to.equal(true);
+            expect(Utils.isArray(new Array())).to.equal(true);
+        });
+        it('should return false if param is not an array', function () {
+            expect(Utils.isArray({})).to.equal(false);
+            expect(Utils.isArray("string")).to.equal(false);
+            expect(Utils.isArray(5)).to.equal(false);
+            expect(Utils.isArray(false)).to.equal(false);
+            expect(Utils.isArray(null)).to.equal(false);
+            expect(Utils.isArray(NaN)).to.equal(false);
+        });
+        it('should return false if param is undefined', function () {
+            expect(Utils.isArray(undefined)).to.equal(false);
+        });
+    });
+    describe('isObject', function() {
+        it('should return true if param is an object', function () {
+            expect(Utils.isObject({})).to.equal(true);
+            expect(Utils.isObject(Object.create({}))).to.equal(true);
+        });
+        it('should return false if param is not an object', function () {
+            expect(Utils.isObject("string")).to.equal(false);
+            expect(Utils.isObject(5)).to.equal(false);
+            expect(Utils.isObject(false)).to.equal(false);
+            expect(Utils.isObject(null)).to.equal(false);
+            expect(Utils.isObject(NaN)).to.equal(false);
+        });
+        it('should return false if param is array', function () {
+            expect(Utils.isObject([])).to.equal(false);
+        });
+        it('should return false if param is undefined', function () {
+            expect(Utils.isObject(undefined)).to.equal(false);
+        });
+    });
+    describe('isBoolean', function() {
+        it('should return true if param is a boolean', function () {
+            expect(Utils.isBoolean(true)).to.equal(true);
+            expect(Utils.isBoolean(false)).to.equal(true);
+            expect(Utils.isBoolean(!!'text')).to.equal(true);
+        });
+        it('should return false if param is not a boolean', function () {
+            expect(Utils.isBoolean([])).to.equal(false);
+            expect(Utils.isBoolean({})).to.equal(false);
+            expect(Utils.isBoolean("string")).to.equal(false);
+            expect(Utils.isBoolean(5)).to.equal(false);
+            expect(Utils.isBoolean(null)).to.equal(false);
+            expect(Utils.isBoolean(NaN)).to.equal(false);
+        });
+        it('should return false if param is undefined', function () {
+            expect(Utils.isBoolean(undefined)).to.equal(false);
+        });
+    });
+    describe('isNumber', function() {
+        it('should return true if param is an int', function () {
+            expect(Utils.isNumber(1)).to.equal(true);
+            expect(Utils.isNumber(-3)).to.equal(true);
+            expect(Utils.isNumber(9999)).to.equal(true);
+        });
+        it('should return true if param is an float', function () {
+            expect(Utils.isNumber(1.2)).to.equal(true);
+        });
+        it('should return false if param is NaN', function () {
+            expect(Utils.isNumber(NaN)).to.equal(false);
+        });
+        it('should return false if param is not a number', function () {
+            expect(Utils.isNumber([])).to.equal(false);
+            expect(Utils.isNumber({})).to.equal(false);
+            expect(Utils.isNumber("string")).to.equal(false);
+            expect(Utils.isNumber(true)).to.equal(false);
+            expect(Utils.isNumber(null)).to.equal(false);
+        });
+        it('should return false if param is undefined', function () {
+            expect(Utils.isNumber(undefined)).to.equal(false);
+        });
+    });
+    describe('isString', function() {
+        it('should return true if param is a string', function () {
+            expect(Utils.isString('string')).to.equal(true);
+        });
+        it('should return false if param is not a string', function () {
+            expect(Utils.isString([])).to.equal(false);
+            expect(Utils.isString({})).to.equal(false);
+            expect(Utils.isString(false)).to.equal(false);
+            expect(Utils.isString(5)).to.equal(false);
+            expect(Utils.isString(null)).to.equal(false);
+            expect(Utils.isString(NaN)).to.equal(false);
+        });
+        it('should return false if param is undefined', function () {
+            expect(Utils.isString(undefined)).to.equal(false);
+        });
+    });
+    describe('isEmpty', function() {
+        it('should be true if object has no keys', function () {
+            expect(Utils.isEmpty({})).to.equal(true);
+        });
+        it('should be false if object has keys', function () {
+            expect(Utils.isEmpty({a:true})).to.equal(false);
+        });
+        it('should be true if array has no length', function () {
+            expect(Utils.isEmpty([])).to.equal(true);
+        });
+        it('should be false if array has items', function () {
+            expect(Utils.isEmpty(["as"])).to.equal(false);
+        });
+        it('should be true if string has no chars', function () {
+            expect(Utils.isEmpty("")).to.equal(false);
+        });
+        it('should be false if string has at least one char', function () {
+            expect(Utils.isEmpty("a")).to.equal(true);
+        });
+    });
 })
